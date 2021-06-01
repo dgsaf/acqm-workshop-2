3d086c1745a3e813bfb16963dfa4b7e9cb352369
Tom Ross <tom.ross@protonmail.com>
2021-06-01 16:07:38 +0800

    [UPDATE] Job script timing

--- jobs.sh
+++ jobs.sh
@@ -3,11 +3,11 @@
 # constant parameters
 alpha_l_const="1.0"
 nuclei_charge="1"
-lambda_max="20"
+lambda_max="10"
 d_r="0.1"
-r_max="100.0"
-d_rz="0.25"
-rz_max="5.0"
+r_max="75.0"
+d_rz="0.5"
+rz_max="6.0"
 
 # parameter sets
 m_set="0"
@@ -35,17 +35,26 @@ echo "> l_max_set: ${l_max_set}"
 echo "> n_basis_l_const_set: ${n_basis_l_const_set}"
 echo "> "
 
-echo "> m, parity, l_max, n_basis_l_const"
+echo "execution:"
+initial=$(date +%s)
 for m in ${m_set} ; do
     for parity in ${parity_set} ; do
         for l_max in ${l_max_set} ; do
             for n_basis_l_const in ${n_basis_l_const_set} ; do
-                echo "${m}, ${parity}, ${l_max}, ${n_basis_l_const}"
+                start=$(date +%s)
+
+                printf "[%i s] " $((start - initial))
+
+                printf "(%i, %i, %i, %3i) " \
+                       ${m} ${parity} ${l_max} ${n_basis_l_const}
 
                 bin/potential_curves ${m} ${parity} ${l_max} \
                                      ${n_basis_l_const} ${alpha_l_const} \
                                      ${nuclei_charge} ${lambda_max} \
                                      ${d_r} ${r_max} ${d_rz} ${rz_max}
+                end=$(date +%s)
+                runtime=$((end - start))
+                printf " [%i s]\n" ${runtime}
 
             done
         done
